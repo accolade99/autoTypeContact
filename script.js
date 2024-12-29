2c2ece39aac2d6c45bfc499b9fb50a722184a1d5
accolade99 <131158528+accolade99@users.noreply.github.com>
2024-12-29 15:17:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const typed = new Typed(".auto-type",{
+    strings:["Teacher", "Web Developer", "Graphic Designer", "Content Creator", "Social Media Manager"],
+    typeSpeed:100,
+    backSpeed:50,
+    loop: true,
+    backDelay: 1500,
+    cursorChar: "||",
+    smartBackspace: true,
+});
